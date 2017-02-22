bede1446806bbf13f64a89877e6ccee3f62fd919
Marley Jade Haupt <marleyjadehaupt@Marleys-MacBook-Air.local>
2017-02-22 13:50:40 -0700

    Odd Dimension Matrix and Populated first 4 corners

--- terrain.R
+++ terrain.R
@@ -1,2 +1,13 @@
 # Terrain is Cool!
 
+start.matrix <- function(dim){
+  if(dim %% 2 == 0){
+    stop("Matrix must have odd dimensions. Odd as in 'not even', not odd as in 'weird'")
+  }
+  terrain.matrix <- matrix(ncol=dim, nrow=dim)
+  terrain.matrix[1,1] <- rnorm(1, 0, 5)
+  terrain.matrix[1,dim] <- rnorm(1, 0, 5)
+  terrain.matrix[dim,1] <- rnorm(1, 0, 5)
+  terrain.matrix[dim,dim] <- rnorm(1, 0, 5)
+  return(terrain.matrix)
+}
