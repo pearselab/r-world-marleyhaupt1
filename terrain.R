c54977cc484d4b7bd69ea028149060d0efbb05f0
Marley Jade Haupt <marleyjadehaupt@Marleys-MacBook-Air.local>
2017-03-06 13:01:01 -0700

    Error in m[1,1]: incorrect number of dimensions

--- terrain.R
+++ terrain.R
@@ -4,12 +4,12 @@
 
 #creates a matrix with odd dimensions and fills the matrix with NAs
 start.matrix <- function(dim){
-  #throws and error message if the dimensions of the matrix aren't odd
+  #error message if the dimensions aren't odd
   if(dim %% 2 == 0){
     stop("Matrix must have odd dimensions")
   }
   m <- matrix(ncol=dim, nrow=dim)
-  #populates the corners of the matrix with random values
+  #populates the corners with random values
   m[1,1] <- rnorm(1, rnorm(1, 0, 1), runif(1, min=0))
   m[1,dim] <- rnorm(1, rnorm(1, 0, 1), runif(1, min=0))
   m[dim,1] <- rnorm(1, rnorm(1, 0, 1), runif(1, min=0))
@@ -17,19 +17,19 @@ start.matrix <- function(dim){
   return(m)
 }
 
-#identifies and fills in the center cell of the matrix
+#identifies and fills center
 diamond.step <- function(m){ 
   tl <- m[1,1]
   tr <- m[1, ncol(m)]
   bl <- m[nrow(m), 1]
   br <- m[nrow(m), ncol(m)]
   mid <- ceiling(nrow(m)/2)
-  #takes the averages of the corners and populates the center cell of the matrix with some noise
+  #averages corners and populates the center cell
   m[mid, mid] <- jitter(mean(c(tl, tr, bl, br)))
   return(m)
 } 
 
-#populates the corners of the new smaller matrices by taking the average of nearby cells with values
+#populates corners
 square.step <- function(m){
   #assign names to key cells
   tl <- m[1,1]
@@ -38,7 +38,7 @@ square.step <- function(m){
   br <- m[nrow(m), ncol(m)]
   mid <- ceiling(nrow(m)/2)
   c <- m[mid, mid]
-  #calculate average and populate cells
+  #calculate average and populates cells
   ml <- jitter(mean(c(tl, c, bl)))
   m[mid, 1] <- ml
   mr <- jitter(mean(c(tr, c, br))) 
@@ -52,7 +52,7 @@ square.step <- function(m){
 
 diamond.square.step <- function(dim){
   m <- start.matrix(dim)
-  #determines the maximum power of 2 based on the dimensions of the matrix specified
+  #maximum power of 2 for the dimensions
   n <- (dim-1)/2
   for(s in 2^(n:1)){
     for(i in seq(from=1, to=s+1, by=s)){ #i is for the rows
@@ -67,7 +67,7 @@ diamond.square.step <- function(dim){
 
 terrain.func <- function(dim, lakes){ 
   m <- diamond.square.step(dim)
-  #fills in the the cells of the matrix with NAs if the value is less than 0
+  #fills cells of matrix with NAs if the value is less than 0
   if(lakes == TRUE){
     for(i in 1:nrow(m)){ 
       for(j in 1:ncol(m)){
