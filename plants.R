cbc0fb03dc0b4420f7a20cb32b5786e59d18cc7f
Marley Jade Haupt <marleyjadehaupt@Marleys-MacBook-Air.local>
2017-03-07 16:21:58 -0700

    Just pluggin' along

--- plants.R
+++ plants.R
@@ -20,9 +20,15 @@ setup.plants <- function(repro, survival, comp.matrix, names=NULL){
   return(list(repro=repro, survival=survival, comp.matrix=comp.matrix))
 }
 
+############################################### NEED TO WORK ON ARRAY MORE ######################
+#make an array of plant locations
+plants <- array("", dim=c(dim(terrain), ts+1))
+for(i in seq_len(dim(plants))[3])
+  plants[,,i][is.na(terrain)] <- NA
+
 survival <- function(cell, info){
   #checks to see if the cell is underwater
-  if(is.na(cell) | cell < 0){
+  if(is.na(cell)){
     cell <- NA
   }
   #checks to see that the cell isn't empty
@@ -33,6 +39,34 @@ survival <- function(cell, info){
       cell <- ""
     }
   }
+  return(cell)
 }
 
-plant.timestep <- function()
+########################################################### NEED TO WORK ON PLANT.TIMESTEP ########
+plant.timestep <- function(plants, terrain, info){
+  #loops through the rows
+  for(i in nrow(plants)){
+    #loops through the columns
+    for(j in ncol(plants)){
+      #applies the survival function to each cell in the matrix
+      survival(plants[i,j], info)
+      reproduce(row=plants[i,], column=plants[,j], plants, info)
+    }
+  }
+  return(new.plants.matrix)
+}
+
+#################################################### REPRO AND COMPETITION NEEDS WORK ############
+reproduce <- function(row, column, plants, info){
+  pos.loc <- as.matrix(expand.grid(row+c(-1,0,1), col+c(-1,0,1)))
+  
+}
+  
+############################################ WORK ON WRAPPER TO SIMULATE PLANTS ON TERRAIN #######
+run.plant.eco <- function(terrain, repro, survive, comp.mat, names=NULL){
+  # include the plant array, survival, reproduction, competition, and plant.timestep functions
+}
+  
+  
+  
+  
