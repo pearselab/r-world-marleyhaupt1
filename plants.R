3b746099fe209596ba8f71a956d8701f1448f57c
Marley Jade Haupt <marleyjadehaupt@Marleys-MacBook-Air.local>
2017-03-13 15:25:35 -0600

    Finished the Run.Plant.Eco Wrapper Function and Added to Plant.Timestep Function

--- plants.R
+++ plants.R
@@ -29,7 +29,10 @@ info <- setup.plants(repro = r, survival = s, comp.matrix = cm, names = names)
 t <- terrain.fun(65)
 
 #initiate plants on the terrain
-init.plants <- function(terrain, timesteps, names){
+init.plants <- function(terrain, timesteps, names=NULL){
+  if(is.null(names)){
+    names <- letters[seq_along(repro)]
+  }
   plants <- array("", dim=c(dim(terrain), timesteps+1))
   for(i in 1:nrow(terrain)){
     for(j in 1:ncol(terrain)){
@@ -42,6 +45,9 @@ init.plants <- function(terrain, timesteps, names){
   }
   return(plants)
 }
+
+#creating a plant array to work with
+plants <- init.plants(terrain = t, timesteps = 2, names=names)
 
 survival <- function(cell, info){
   #checks to see if the cell is underwater
@@ -60,28 +66,35 @@ survival <- function(cell, info){
 }
 
 ########################################################### NEED TO WORK ON PLANT.TIMESTEP ########
-plant.timestep <- function(plants, terrain, info){
-  #loops through the rows
-  for(i in nrow(plants)){
-    #loops through the columns
-    for(j in ncol(plants)){
-      #applies the survival function to each cell in the matrix
-      survival(plants[i,j], info)
-      reproduce(row=plants[i,], column=plants[,j], plants, info)
-    }
-  }
-  return(new.plants.matrix)
-}
+ 
 
 #################################################### REPRO AND COMPETITION NEEDS WORK ############
 reproduce <- function(row, column, plants, info){
   pos.loc <- as.matrix(expand.grid(row+c(-1,0,1), col+c(-1,0,1)))
   
 }
+
+plant.timestep <- function(plants, terrain, info, timesteps){
+  #loops through the timesteps
+  for(k in 1:(timesteps+1)){
+    #loops through the rows
+    for(i in nrow(plants)){
+      #loops through the columns
+      for(j in ncol(plants)){
+        #applies the survival function to each cell in the matrix
+        survival(plants[i,j], info)
+        reproduce(row=plants[i,], column=plants[,j], plants, info)
+      }
+    }
+  }
+  return(new.plants.matrix)
+}
   
-############################################ WORK ON WRAPPER TO SIMULATE PLANTS ON TERRAIN #######
-run.plant.eco <- function(timesteps, terrain, repro, survive, comp.mat, names=NULL){
-  # include the init.plant, survival, reproduction/competition, and plant.timestep functions
+run.plant.eco <- function(timesteps, terrain, repro, survival, comp.matrix, names=NULL){
+  info <- setup.plants(repro=repro, survival=survival, comp.matrix=comp.matrix, names = names)
+  plants <- init.plants(terrain = terrain, timesteps = timesteps, names = names)
+  output <- plant.timestep(plants=plants, terrain=terrain, info=info, timesteps=timesteps)
+  return(output)
 }
   
   
